282814c89fd64ee300e2e2dc26996f147bd57876
ppeirce <ppeirce@10.0.1.7>
2018-10-11 18:18:24 -0400

    Class for data file management

---
+++ hw2/src/edu/nyu/cs/pa/hw2/DataFiles.java
@@ -0,0 +1,38 @@
+package edu.nyu.cs.pa.hw2;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class DataFiles {
+    static final File DATA_C11 = new File("data/C1/article01.txt");
+    static final File DATA_C12 = new File("data/C1/article02.txt");
+    static final File DATA_C13 = new File("data/C1/article03.txt");
+    static final File DATA_C14 = new File("data/C1/article04.txt");
+    static final File DATA_C15 = new File("data/C1/article05.txt");
+    static final File DATA_C16 = new File("data/C1/article06.txt");
+    static final File DATA_C17 = new File("data/C1/article07.txt");
+    static final File DATA_C18 = new File("data/C1/article08.txt");
+
+    static final File DATA_C41 = new File("data/C4/article01.txt");
+    static final File DATA_C42 = new File("data/C4/article02.txt");
+    static final File DATA_C43 = new File("data/C4/article03.txt");
+    static final File DATA_C44 = new File("data/C4/article04.txt");
+    static final File DATA_C45 = new File("data/C4/article05.txt");
+    static final File DATA_C46 = new File("data/C4/article06.txt");
+    static final File DATA_C47 = new File("data/C4/article07.txt");
+    static final File DATA_C48 = new File("data/C4/article08.txt");
+
+    static final File DATA_C71 = new File("data/C7/article01.txt");
+    static final File DATA_C72 = new File("data/C7/article02.txt");
+    static final File DATA_C73 = new File("data/C7/article03.txt");
+    static final File DATA_C74 = new File("data/C7/article04.txt");
+    static final File DATA_C75 = new File("data/C7/article05.txt");
+    static final File DATA_C76 = new File("data/C7/article06.txt");
+    static final File DATA_C77 = new File("data/C7/article07.txt");
+    static final File DATA_C78 = new File("data/C7/article08.txt");
+    
+    static final File[] FILES = {
+            DATA_C11, DATA_C12, DATA_C13, DATA_C14, DATA_C15, DATA_C16, DATA_C17, DATA_C18,
+            DATA_C41, DATA_C42, DATA_C43, DATA_C44, DATA_C45, DATA_C46, DATA_C47, DATA_C48,
+            DATA_C71, DATA_C72, DATA_C73, DATA_C74, DATA_C75, DATA_C76, DATA_C77, DATA_C78};
+}
