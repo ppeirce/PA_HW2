a353a4234af4bc03bebeb283242960dd08f118e3
ppeirce <ppeirce@10.0.1.7>
2018-10-12 15:44:41 -0400

    remove unnecessary imports from driver

--- hw2/src/edu/nyu/cs/pa/hw2/TextMiningDriver.java
+++ hw2/src/edu/nyu/cs/pa/hw2/TextMiningDriver.java
@@ -1,11 +1,7 @@
 package edu.nyu.cs.pa.hw2;
 
-import edu.stanford.nlp.simple.Document;
-import edu.stanford.nlp.simple.Sentence;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
 
 public class TextMiningDriver {
     public static void main(String[] args) throws IOException {                
