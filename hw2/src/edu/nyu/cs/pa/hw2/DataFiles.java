56e8dca5ebfeee648b7591f874c7f0a9ed73c075
ppeirce <ppeirce@10.0.1.7>
2018-10-12 13:56:16 -0400

    add files
    might be unnecessary but idc

--- hw2/src/edu/nyu/cs/pa/hw2/DataFiles.java
+++ hw2/src/edu/nyu/cs/pa/hw2/DataFiles.java
@@ -4,32 +4,32 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class DataFiles {
-    static final File DATA_C11 = new File("data/C1/article01.txt");
-    static final File DATA_C12 = new File("data/C1/article02.txt");
-    static final File DATA_C13 = new File("data/C1/article03.txt");
-    static final File DATA_C14 = new File("data/C1/article04.txt");
-    static final File DATA_C15 = new File("data/C1/article05.txt");
-    static final File DATA_C16 = new File("data/C1/article06.txt");
-    static final File DATA_C17 = new File("data/C1/article07.txt");
-    static final File DATA_C18 = new File("data/C1/article08.txt");
+    static final File DATA_C11 = new File("data/original/C1/article01.txt");
+    static final File DATA_C12 = new File("data/original/C1/article02.txt");
+    static final File DATA_C13 = new File("data/original/C1/article03.txt");
+    static final File DATA_C14 = new File("data/original/C1/article04.txt");
+    static final File DATA_C15 = new File("data/original/C1/article05.txt");
+    static final File DATA_C16 = new File("data/original/C1/article06.txt");
+    static final File DATA_C17 = new File("data/original/C1/article07.txt");
+    static final File DATA_C18 = new File("data/original/C1/article08.txt");
 
-    static final File DATA_C41 = new File("data/C4/article01.txt");
-    static final File DATA_C42 = new File("data/C4/article02.txt");
-    static final File DATA_C43 = new File("data/C4/article03.txt");
-    static final File DATA_C44 = new File("data/C4/article04.txt");
-    static final File DATA_C45 = new File("data/C4/article05.txt");
-    static final File DATA_C46 = new File("data/C4/article06.txt");
-    static final File DATA_C47 = new File("data/C4/article07.txt");
-    static final File DATA_C48 = new File("data/C4/article08.txt");
+    static final File DATA_C41 = new File("data/original/C4/article01.txt");
+    static final File DATA_C42 = new File("data/original/C4/article02.txt");
+    static final File DATA_C43 = new File("data/original/C4/article03.txt");
+    static final File DATA_C44 = new File("data/original/C4/article04.txt");
+    static final File DATA_C45 = new File("data/original/C4/article05.txt");
+    static final File DATA_C46 = new File("data/original/C4/article06.txt");
+    static final File DATA_C47 = new File("data/original/C4/article07.txt");
+    static final File DATA_C48 = new File("data/original/C4/article08.txt");
 
-    static final File DATA_C71 = new File("data/C7/article01.txt");
-    static final File DATA_C72 = new File("data/C7/article02.txt");
-    static final File DATA_C73 = new File("data/C7/article03.txt");
-    static final File DATA_C74 = new File("data/C7/article04.txt");
-    static final File DATA_C75 = new File("data/C7/article05.txt");
-    static final File DATA_C76 = new File("data/C7/article06.txt");
-    static final File DATA_C77 = new File("data/C7/article07.txt");
-    static final File DATA_C78 = new File("data/C7/article08.txt");
+    static final File DATA_C71 = new File("data/original/C7/article01.txt");
+    static final File DATA_C72 = new File("data/original/C7/article02.txt");
+    static final File DATA_C73 = new File("data/original/C7/article03.txt");
+    static final File DATA_C74 = new File("data/original/C7/article04.txt");
+    static final File DATA_C75 = new File("data/original/C7/article05.txt");
+    static final File DATA_C76 = new File("data/original/C7/article06.txt");
+    static final File DATA_C77 = new File("data/original/C7/article07.txt");
+    static final File DATA_C78 = new File("data/original/C7/article08.txt");
     
     static final File[] FILES = {
             DATA_C11, DATA_C12, DATA_C13, DATA_C14, DATA_C15, DATA_C16, DATA_C17, DATA_C18,
